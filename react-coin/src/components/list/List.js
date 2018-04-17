f38af9e0af14d24f941fa64c57b7db00db254e0e
Will Hinton <will.h.hinton@gmail.com>
2018-04-17 18:46:52 -0500

    Added pagination functionality

--- react-coin/src/components/list/List.js
+++ react-coin/src/components/list/List.js
@@ -3,6 +3,7 @@ import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
 import Loading from '../common/Loading';
 import Table from './Table';
+import Pagination from './Pagination';
 
 class List extends React.Component {
   constructor() {
@@ -12,18 +13,31 @@ class List extends React.Component {
       loading: false,
       currencies: [],
       error: null,
+      totalPages: 0,
+      page: 1,
     };
+
+    this.handlePaginationClick = this.handlePaginationClick.bind(this);
   }
 
   componentDidMount() {
+    this.fetchCurrencies();
+  }
+
+  fetchCurrencies() {
     this.setState({
       loading: true});
 
-    fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
+    const { page } = this.state;
+
+    fetch(`${API_URL}/cryptocurrencies?page=${page}&perPage=20`)
     .then(handleResponse)
     .then((data) => {
+      const { currencies, totalPages} = data;
+
       this.setState({
-        currencies : data.currencies,
+        currencies : currencies,
+        totalPages: totalPages,
         loading: false});
     })
     .catch((error) => {
@@ -43,8 +57,20 @@ class List extends React.Component {
     }
   }
 
+  handlePaginationClick(direction){
+    let nextPage = this.state.page;
+
+    //increment nextPage if direction variable is next, otherwise decrement
+    nextPage = direction === 'next' ? nextPage + 1: nextPage - 1;
+
+    this.setState({page: nextPage}, () => {
+      //call fetchCurrencies funciton inside callback to make sure page is updated
+      this.fetchCurrencies();
+    });
+  }
+
   render() {
-    const { loading, error, currencies} = this.state;
+    const { loading, error, currencies, page, totalPages} = this.state;
 
 
     //render only loading component if loading state is set to true
@@ -58,10 +84,18 @@ class List extends React.Component {
     }
 
     return (
-      <Table
-        currencies={currencies}
-        renderChangePercent={this.renderChangePercent}
-       />
+      <div>
+        <Table
+          currencies={currencies}
+          renderChangePercent={this.renderChangePercent}
+        />
+
+        <Pagination
+          page={page}
+          totalPages={totalPages}
+          handlePaginationClick={this.handlePaginationClick}
+        />
+      </div>
     );
   }
 }
