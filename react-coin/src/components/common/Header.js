d9e1af30f8c4fcabe2a9d780b114c4f0a6b5e566
Will Hinton <will.h.hinton@gmail.com>
2018-04-19 13:03:34 -0500

    Revert "Added search component and modified loading feature"
    This reverts commit 28f45aa2f8f75b0f69c10e5099ec3c4c6825cd6f.

--- react-coin/src/components/common/Header.js
+++ react-coin/src/components/common/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Search from './Search';
 import logo from './logo.png';
 import './Header.css';
 
@@ -10,8 +9,6 @@ const Header = () => {
 			<Link to="/">
 				<img src={logo} alt="logo" className="Header-logo" />
 			</Link>
-
-			<Search />
 		</div>
 		);
 }
--- react-coin/src/components/common/Loading.js
+++ react-coin/src/components/common/Loading.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Loading.css';
 
-const Loading = (props) => {
-  const { width, height } = props;
-
-  return <div className="Loading" style={{ width, height }} />;
+const Loading = () => {
+  return <div className="Loading" />;
 }
-
-Loading.defaultProps = {
-  width: '28px',
-  height: '28px',
-}
-
-Loading.propTypes = {
-  width: PropTypes.string,
-  height: PropTypes.string,
-};
 
 export default Loading;
